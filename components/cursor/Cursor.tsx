f4a32e75b68284a1fce40f50c6994d5052baaaa0
Grant Li <ligrant1012@gmail.com>
2024-02-26 20:04:04 -0800

    cursor chat

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -14,7 +14,14 @@ const Cursor = ({color, x, y, message}: Props) => {
                 transform: `translateX(${x}px) translateY(${y}px)`
             }}>
             <CursorSVG color={color} />
-            {/*TODO: MESSAGE */}
+            {message && (
+                <div
+                    className="absolute left-2 top-5 rounded-3xl px-4 py-2"
+                    style={{ backgroundColor: color }}
+                >
+                    <p className="text-white whitespace-nowrap text-sm leading-relaxed">{message}</p>
+                </div>
+            )}
         </div>
     )
 }
